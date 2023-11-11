782c272dd73deeae1c88a791125b02349fd9af75
Derek <alexanderderekrein@gmail.com>
2023-11-11 21:14:14 +0100

    feat: link to contract

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,9 @@ import { MintUSDC } from "@/components/MintUSDC";
 import Toggle from 'react-toggle'
 import { MintSmart } from "@/components/MintSmart";
 import { MintSmartUSDC } from "@/components/MintSmartUSDC";
+import constants from '../utils/constants'
+
+const contractAddress = constants.contractAddreses['polygon'] as `0x${string}`;
 
 export default function Home() {
 	const [isNetworkSwitchHighlighted, setIsNetworkSwitchHighlighted] =
@@ -109,12 +112,12 @@ export default function Home() {
 								d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
 							/>
 						</svg>
-						{/* <a
-							href="https://docs.walletconnect.com/web3modal/react/about?utm_source=next-starter-template&utm_medium=github&utm_campaign=next-starter-template"
+						<a
+							href={`https://polygonscan.com/address/${contractAddress}`}
 							target="_blank"
 						>
-							TBD ADD link
-						</a> */}
+							Polygon Contract
+						</a>
 					</div>
 				</div>
 			</main>
